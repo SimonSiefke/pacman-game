028526651e279f1e1c97672c51f599a1c63c24d6
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2018-12-18 11:12:27 +0100

    responsive changes

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,16 @@
     <RouterView/>
   </div>
 </template>
+
+<style lang="stylus">
+#app
+  display block
+  height 100%
+  overflow hidden
+  position relative
+  width 100%
+
+  .game
+    max-height 100%
+    max-width 100%
+</style>
